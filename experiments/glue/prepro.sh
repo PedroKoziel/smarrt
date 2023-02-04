12a9856e055cdae9861b4ce5c1f7e55a524cf9d0
PedroKoziel <85353752+PedroKoziel@users.noreply.github.com>
2023-02-04 10:16:14 -0300

    Update prepro.sh

--- experiments/glue/prepro.sh
+++ experiments/glue/prepro.sh
@@ -6,7 +6,7 @@
 ############################### 
 
 ## dump original data into tsv
-python experiments/glue/glue_prepro.py
+python /content/smarrt/experiments/glue/glue_prepro.py
 
 
 declare -a PLMS=('bert-base-uncased' 'roberta-base' 'microsoft/deberta-base' 't5-base')
@@ -15,7 +15,8 @@ declare -a PLMS=('bert-base-uncased' 'roberta-base' 'microsoft/deberta-base' 't5
 for plm in "${PLMS[@]}"
 do
   echo "Prepro GLUE data for $plm"
-  python prepro_std.py --model $plm --root_dir data/canonical_data --task_def experiments/glue/glue_task_def.yml --workers 32
+  python /content/smarrt/prepro_std.py --model $plm --root_dir data/canonical_data --task_def /content/smarrt/experiments/glue/glue_task_def.yml --workers 32
 done
+
 
 
